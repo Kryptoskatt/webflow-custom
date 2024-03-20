98c9ef1bd2f9095f6f009b2387e67507d3aa1566
hazel-ish <163507784+hazel-ish@users.noreply.github.com>
2024-03-20 17:59:15 +0530

    Update TrackHeaderSignUp.js

--- TrackHeaderSignUp.js
+++ TrackHeaderSignUp.js
@@ -1,4 +1,5 @@
 var signUpCTA = document.getElementById('header_sign_up');
+var signInCTA = document.getElementById('header_sign_in');
 var pageUrl = window.location.href;
 var pageName = pageUrl.substring(pageUrl.lastIndexOf('/') + 1);
 // If pageName is empty (no characters after the last '/'), set it to 'Home'
@@ -11,4 +12,11 @@ mixpanel.track('Header Sign Up', {
 'Page Name': pageName
 });
 });
+signInCTA.addEventListener('click', function() {
+mixpanel.track('Header Sign In', {
+'Page URL': pageUrl,
+'Page Name': pageName
+});
+});
+
 
