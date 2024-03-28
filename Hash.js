6ab4744d4e094d64fe735e6d8ab25476fcaca2f9
hazel-ish <163507784+hazel-ish@users.noreply.github.com>
2024-03-29 01:39:08 +0530

    Update Hash.js

--- Hash.js
+++ Hash.js
@@ -1,17 +1,17 @@
 async function getLatestCommitSHA(owner, repo) {
-  const url = `https://api.github.com/repos/${owner}/${repo}/commits`;
-  try{
-  const response = await fetch(url);
-  console.log("response", response);
-  if(response){
-      console.log(response);
-  }
-}catch(error){
-  console.log("some error occured",error);
+const url = `https://api.github.com/repos/${owner}/${repo}/commits`;
+try{
+const response = await fetch(url);
+const finalResponse = await response.json();
+if(finalResponse){
+    console.log(finalResponse);
 }
-  }
-  
-  const owner = 'Kryptoskatt';
-  const repo = 'webflow-custom';
-  
-  getLatestCommitSHA(owner,repo);
+}catch(error){
+console.log("some error occured",error);
+}
+}
+
+const owner = 'Kryptoskatt';
+const repo = 'webflow-custom';
+
+getLatestCommitSHA(owner,repo);
