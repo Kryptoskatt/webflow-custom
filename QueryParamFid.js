540b9c2624016b998ca8170e7848798f430e0283
hazel-ish <163507784+hazel-ish@users.noreply.github.com>
2024-03-29 01:54:52 +0530

    Update QueryParamFid.js

--- QueryParamFid.js
+++ QueryParamFid.js
@@ -8,9 +8,7 @@ return ('0' + byte.toString(16)).slice(-2);
 }
 
 // Add event listener to all elements with the custom attribute 'sign_up'
-console.log("before event is triggered Query Param");
 document.addEventListener('click', function(event) {
-console.log("click event is triggered queryparam", event);
 // Check if the clicked element has the custom attribute 'sign_up'
 var signUpTarget = event.target.getAttribute('sign_up');
 if (signUpTarget !== null) {
