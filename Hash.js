2aed6dbd1b1ba27357ac744ad34a3de32029dc32
hazel-ish <163507784+hazel-ish@users.noreply.github.com>
2024-03-29 01:29:19 +0530

    Update Hash.js

--- Hash.js
+++ Hash.js
@@ -1,7 +1,7 @@
 async function getLatestCommitSHA(owner, repo) {
 const url = `https://api.github.com/repos/${owner}/${repo}/commits`;
 const response = await fetch(url);
-console.log(response);
+console.log("response", response);
 if(response){
     console.log(response);
 }
