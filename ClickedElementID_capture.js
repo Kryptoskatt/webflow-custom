da1a54f1e9c38d396992d234aeddf940ca3b21d4
hazel-ish <163507784+hazel-ish@users.noreply.github.com>
2024-03-29 00:53:47 +0530

    Update ClickedElementID_capture.js

--- ClickedElementID_capture.js
+++ ClickedElementID_capture.js
@@ -1,17 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
 console.log("before event is triggered Datatracker ClickedElement ID");
 document.addEventListener('click', function(event) {
 console.log("event is triger",event);
 var clickedElements = localStorage.getItem('clickedKey');
 console.log("clickedKey",clickedElements);
 if(clickedElements==='' || clickedElements==null || clickedElements==undefined){
-    clickedElements=[];
+clickedElements=[];
 }else{
-    clickedElements=JSON.parse(clickedElements);
+clickedElements=JSON.parse(clickedElements);
 }
 var clickedElementId = event.target.id;
 if (clickedElementId) {
-    if(clickedElementId!=='search' && clickedElementId!=='name' && clickedElementId!=='radio' && clickedElementId!=='marketing-2' && clickedElementId!=='analytics-2' && clickedElementId!=='personalization-2' && clickedElementId!=='w-dropdown-toggle-0')
-    clickedElements.push(clickedElementId);
-    localStorage.setItem('clickedKey', JSON.stringify(clickedElements));
+if(clickedElementId!=='search' && clickedElementId!=='name' && clickedElementId!=='radio' && clickedElementId!=='marketing-2' && clickedElementId!=='analytics-2' && clickedElementId!=='personalization-2' && clickedElementId!=='w-dropdown-toggle-0')
+clickedElements.push(clickedElementId);
+localStorage.setItem('clickedKey', JSON.stringify(clickedElements));
 }
+});
 });
