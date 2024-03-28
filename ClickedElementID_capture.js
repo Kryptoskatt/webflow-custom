ba5633fb8f3026e3be7aadc4beba0caf5af01380
hazel-ish <163507784+hazel-ish@users.noreply.github.com>
2024-03-29 00:15:33 +0530

    Update ClickedElementID_capture.js

--- ClickedElementID_capture.js
+++ ClickedElementID_capture.js
@@ -1,14 +1,16 @@
 document.addEventListener('click', function(event) {
-    var clickedElements = localStorage.getItem('clickedKey');
-    if(clickedElements==='' || clickedElements==null || clickedElements==undefined){
-        clickedElements=[];
-    }else{
-        clickedElements=JSON.parse(clickedElements);
-    }
-    var clickedElementId = event.target.id;
-    if (clickedElementId) {
-        if(clickedElementId!=='search' && clickedElementId!=='name' && clickedElementId!=='radio' && clickedElementId!=='marketing-2' && clickedElementId!=='analytics-2' && clickedElementId!=='personalization-2' && clickedElementId!=='w-dropdown-toggle-0')
-        clickedElements.push(clickedElementId);
-        localStorage.setItem('clickedKey', JSON.stringify(clickedElements));
-    }
+console.log("event is triger",event);
+var clickedElements = localStorage.getItem('clickedKey');
+console.log("clickedKey",clickedElements);
+if(clickedElements==='' || clickedElements==null || clickedElements==undefined){
+    clickedElements=[];
+}else{
+    clickedElements=JSON.parse(clickedElements);
+}
+var clickedElementId = event.target.id;
+if (clickedElementId) {
+    if(clickedElementId!=='search' && clickedElementId!=='name' && clickedElementId!=='radio' && clickedElementId!=='marketing-2' && clickedElementId!=='analytics-2' && clickedElementId!=='personalization-2' && clickedElementId!=='w-dropdown-toggle-0')
+    clickedElements.push(clickedElementId);
+    localStorage.setItem('clickedKey', JSON.stringify(clickedElements));
+}
 });
