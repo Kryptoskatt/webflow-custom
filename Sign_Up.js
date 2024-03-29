badbc8b0084549664ceb390108d85b00dff964e4
hazel-ish <163507784+hazel-ish@users.noreply.github.com>
2024-03-29 13:36:19 +0530

    Update Sign_Up.js

--- Sign_Up.js
+++ Sign_Up.js
@@ -1,8 +1,9 @@
-
 document.addEventListener('click', function(event) {
 
 var pageUrl = window.location.href;
-var pageName = pageUrl.substring(pageUrl.lastIndexOf('/') + 1);
+var queryIndex = pageUrl.indexOf('?');
+var croppedPageUrl = queryIndex !== -1 ? pageUrl.substring(0, queryIndex) : pageUrl;
+var pageName = croppedPageUrl.substring(croppedPageUrl.lastIndexOf('/') + 1);
 
 if (pageName === '') {
 pageName = 'Home';
@@ -13,7 +14,7 @@ var signUpValue = event.target.getAttribute('sign_up');
 
 mixpanel.track('Sign Up', {
 'CTA Name': signUpValue,
-'Page URL': pageUrl,
+'Page URL': croppedPageUrl,
 'Page Name': pageName
 });
 }
@@ -22,7 +23,7 @@ if (event.target.getAttribute('sign_in') !== null) {
 var signInValue = event.target.getAttribute('sign_in');
 mixpanel.track('Sign In', {
 'CTA Name': signInValue,
-'Page URL': pageUrl,
+'Page URL': croppedPageUrl,
 'Page Name': pageName
 });
 }
