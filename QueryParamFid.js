09eba940cbb4aa9aabbe8a3bf75bee226aad14c0
hazel-ish <163507784+hazel-ish@users.noreply.github.com>
2024-04-16 17:16:14 +0530

    Update QueryParamFid.js

--- QueryParamFid.js
+++ QueryParamFid.js
@@ -1,4 +1,3 @@
-console.log("dom file");
 // Function to generate a cryptographically secure random hexadecimal string
 function generateRandomHex(length) {
 var array = new Uint8Array(length / 2);
@@ -7,14 +6,13 @@ return Array.from(array, function(byte) {
 return ('0' + byte.toString(16)).slice(-2);
 }).join('');
 }
-console.log("query param file");
+
 // Add event listener to all elements with the custom attribute 'sign_up'
 document.addEventListener('click', function(event) {
-console.log("click event happened");
+
 // Check if the clicked element has the custom attribute 'sign_up'
 var signUpTarget = event.target.getAttribute('sign_up');
 if (signUpTarget !== null) {
-  console.log("signUpTarget", signUpTarget);
 // Retrieve the target URL from the href attribute of the clicked <a> tag
 var targetUrl = event.target.getAttribute('href');
 // Generate random hex
